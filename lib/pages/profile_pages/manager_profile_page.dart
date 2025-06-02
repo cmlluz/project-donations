aadf077406bd72d7ad1b57a4de6144cdfc6cdeb6
Luana Lima <luanalima.2006@hotmail.com>
2025-06-02 11:10:47 -0300

    style: restyle personal profile info

--- lib/pages/profile_pages/manager_profile_page.dart
+++ lib/pages/profile_pages/manager_profile_page.dart
@@ -17,57 +17,72 @@ class _ManagerProfilePage extends State<ManagerProfilePage> {
         backgroundColor: ConstantsColors.whiteShade900,
         body: SingleChildScrollView(
           padding: EdgeInsets.all(16.0),
-          child: Column(
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Center(
-                child: CircleAvatar(
+              Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: ConstantsColors.blueShade900,
+                    width: 4.0,
+                  ),
+                ),
+                child: const CircleAvatar(
+                  radius: 70,
                   backgroundImage: NetworkImage(
                     "https://media.gettyimages.com/id/1317804578/pt/foto/one-businesswoman-headshot-smiling-at-the-camera.jpg?s=612x612&w=0&k=20&c=RXbgBRAoPeDrPXNLXI74Th6Lexbk6PRQ6q0b4rIzEcc=",
                   ),
-                  radius: 70,
                 ),
               ),
-              const SizedBox(height: 40),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              const SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 5),
+                    Text(
+                      'Lucia Fontes ',
+                      style: TextStylesConstants.kinterSemiBold
+                          .merge(const TextStyle(fontSize: 20.0)),
+                    ),
+                    const SizedBox(height: 3),
+                    Text(
+                      'Contato: ',
+                      style: TextStylesConstants.kinterBold
+                          .merge(const TextStyle(fontSize: 15.0)),
+                    ),
+                    const SizedBox(height: 4),
+                    Row(
                       children: [
+                        const Icon(Icons.phone_in_talk_outlined,
+                            size: 20.0, color: ConstantsColors.blackShade700),
+                        const SizedBox(width: 6),
                         Text(
-                          'Nome: ',
-                          style: TextStylesConstants.kpoppinsLight
-                              .merge(TextStylesConstants.kTitle),
-                        ),
-                        const Text(
-                          'Lucia Fontes ',
-                          style: TextStylesConstants.kformularyText,
-                        ),
-                        const SizedBox(height: 20),
-                        const Text(
-                          'E-mail: ',
-                          style: TextStylesConstants.kTitleProfile,
-                        ),
-                        const Text(
-                          'Luciafontes@gmail.com',
-                          style: TextStylesConstants.kformularyText,
-                        ),
-                        const SizedBox(height: 20),
-                        const Text(
-                          'Telefone: ',
-                          style: TextStylesConstants.kTitleProfile,
-                        ),
-                        const Text(
                           '(71)12345-6789',
-                          style: TextStylesConstants.kformularyText,
+                          style: TextStylesConstants.kinterRegular
+                              .merge(const TextStyle(fontSize: 15.0)),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 4),
+                    Row(
+                      children: [
+                        const Icon(Icons.email_outlined,
+                            size: 20.0, color: ConstantsColors.blackShade700),
+                        const SizedBox(width: 6),
+                        Text(
+                          'Luciafontes@gmail.com',
+                          style: TextStylesConstants.kinterRegular
+                              .merge(const TextStyle(fontSize: 15.0)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
