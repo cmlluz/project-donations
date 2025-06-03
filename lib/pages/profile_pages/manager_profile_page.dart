fba56c6f8e72c7ed1ba7f0a0d3f7a6e3f586a547
Luana Lima <luanalima.2006@hotmail.com>
2025-06-03 11:02:06 -0300

    feat: add details info to personal page

--- lib/pages/profile_pages/manager_profile_page.dart
+++ lib/pages/profile_pages/manager_profile_page.dart
@@ -21,73 +21,93 @@ class _ManagerProfilePage extends State<ManagerProfilePage> {
             SingleChildScrollView(
               padding:
                   const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
-              child: Row(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    width: 120,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: ConstantsColors.blueShade900,
-                        width: 4.0,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: ConstantsColors.blueShade900,
+                            width: 4.0,
+                          ),
+                        ),
+                        child: const CircleAvatar(
+                          radius: 70,
+                          backgroundImage: NetworkImage(
+                            "https://media.gettyimages.com/id/1317804578/pt/foto/one-businesswoman-headshot-smiling-at-the-camera.jpg?s=612x612&w=0&k=20&c=RXbgBRAoPeDrPXNLXI74Th6Lexbk6PRQ6q0b4rIzEcc=",
+                          ),
+                        ),
                       ),
-                    ),
-                    child: const CircleAvatar(
-                      radius: 70,
-                      backgroundImage: NetworkImage(
-                        "https://media.gettyimages.com/id/1317804578/pt/foto/one-businesswoman-headshot-smiling-at-the-camera.jpg?s=612x612&w=0&k=20&c=RXbgBRAoPeDrPXNLXI74Th6Lexbk6PRQ6q0b4rIzEcc=",
+                      const SizedBox(width: 15),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 5),
+                            Text(
+                              'Lucia Fontes ',
+                              style: TextStylesConstants.kinterSemiBold
+                                  .merge(const TextStyle(fontSize: 20.0)),
+                            ),
+                            const SizedBox(height: 3),
+                            Text(
+                              'Contato: ',
+                              style: TextStylesConstants.kinterBold
+                                  .merge(const TextStyle(fontSize: 15.0)),
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              children: [
+                                const Icon(Icons.phone_in_talk_outlined,
+                                    size: 20.0,
+                                    color: ConstantsColors.blackShade700),
+                                const SizedBox(width: 6),
+                                Text(
+                                  '(71)12345-6789',
+                                  style: TextStylesConstants.kinterRegular
+                                      .merge(const TextStyle(fontSize: 15.0)),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              children: [
+                                const Icon(Icons.email_outlined,
+                                    size: 20.0,
+                                    color: ConstantsColors.blackShade700),
+                                const SizedBox(width: 6),
+                                Text(
+                                  'Luciafontes@gmail.com',
+                                  style: TextStylesConstants.kinterRegular
+                                      .merge(const TextStyle(fontSize: 15.0)),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  Text(
+                    'Detalhes:',
+                    style: TextStylesConstants.kpoppinsMedium.merge(
+                      const TextStyle(fontSize: 20.0),
                     ),
                   ),
-                  const SizedBox(width: 15),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 5),
-                        Text(
-                          'Lucia Fontes ',
-                          style: TextStylesConstants.kinterSemiBold
-                              .merge(const TextStyle(fontSize: 20.0)),
-                        ),
-                        const SizedBox(height: 3),
-                        Text(
-                          'Contato: ',
-                          style: TextStylesConstants.kinterBold
-                              .merge(const TextStyle(fontSize: 15.0)),
-                        ),
-                        const SizedBox(height: 4),
-                        Row(
-                          children: [
-                            const Icon(Icons.phone_in_talk_outlined,
-                                size: 20.0,
-                                color: ConstantsColors.blackShade700),
-                            const SizedBox(width: 6),
-                            Text(
-                              '(71)12345-6789',
-                              style: TextStylesConstants.kinterRegular
-                                  .merge(const TextStyle(fontSize: 15.0)),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 4),
-                        Row(
-                          children: [
-                            const Icon(Icons.email_outlined,
-                                size: 20.0,
-                                color: ConstantsColors.blackShade700),
-                            const SizedBox(width: 6),
-                            Text(
-                              'Luciafontes@gmail.com',
-                              style: TextStylesConstants.kinterRegular
-                                  .merge(const TextStyle(fontSize: 15.0)),
-                            ),
-                          ],
-                        ),
-                      ],
+                  const SizedBox(height: 8),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ali',
+                    style: TextStylesConstants.kpoppinsMedium.merge(
+                      const TextStyle(
+                          fontSize: 14.0, color: ConstantsColors.greyShade600),
                     ),
                   ),
                 ],
