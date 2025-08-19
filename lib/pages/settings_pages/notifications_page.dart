00c933ef2f9fd3d10085a14d0a9f11bd746932cf
Luana Lima <luanalima.2006@hotmail.com>
2025-08-18 23:06:32 -0300

    feat: create page for activating notifications

--- lib/pages/settings_pages/notifications_page.dart
+++ lib/pages/settings_pages/notifications_page.dart
@@ -1,13 +1,7 @@
-// import 'package:appdonationsgestor/pages/settings_pages/remove_account_page.dart';
-// import 'package:appdonationsgestor/pages/settings_pages/edit_profile_page.dart';
-import 'package:appdonationsgestor/auth/app_data.dart';
-import 'package:appdonationsgestor/auth/auth_service.dart';
 import 'package:appdonationsgestor/resources/constant_colors.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:appdonationsgestor/resources/text_styles.dart';
 import 'package:go_router/go_router.dart';
-import 'package:appdonationsgestor/controllers/navigation_controller.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -30,7 +24,7 @@ class _NotificationsPage extends State<NotificationsPage> {
           const TextStyle(
             color: ConstantsColors.blueShade900,
             fontSize: 24,
-          ),
+          ).merge(TextStylesConstants.kpoppinsRegular),
         ),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios),
@@ -39,95 +33,60 @@ class _NotificationsPage extends State<NotificationsPage> {
         ),
       ),
       backgroundColor: ConstantsColors.whiteShade900,
-      // body: SingleChildScrollView(
-      //   child: Column(
-      //     children: [
-      //       _buildSettingOption(
-      //         title: 'Editar Perfil',
-      //         onTap: () {
-      //           GoRouter.of(context).pushNamed('editProfilePage');
-      //         },
-      //       ),
-      //       _buildSettingOption(
-      //         title: 'Notificações',
-      //         icon: isNotificationOn
-      //             ? Icons.toggle_on
-      //             : Icons.toggle_off_outlined,
-      //         iconColor: isNotificationOn
-      //             ? ConstantsColors.blueShade900
-      //             : ConstantsColors.blueShade900,
-      //         iconSize: 36,
-      //         alignment: MainAxisAlignment.spaceBetween,
-      //         onTap: () {
-      //           setState(() {
-      //             isNotificationOn = !isNotificationOn;
-      //           });
-      //         },
-      //       ),
-      //       _buildSettingOption(
-      //         title: 'Deletar conta',
-      //         // titleSize: 18,
-      //         textColor: ConstantsColors.redShade800,
-      //         onTap: () {
-      //           GoRouter.of(context).pushNamed('removeAccountPage');
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
-    );
-  }
-
-  Widget _buildSettingOption({
-    required String title,
-    double titleSize = 16,
-    IconData? icon,
-    Color textColor = ConstantsColors.blackShade900,
-    Color iconColor = ConstantsColors.blackShade900,
-    double iconSize = 28,
-    required VoidCallback onTap,
-    MainAxisAlignment alignment = MainAxisAlignment.start,
-  }) {
-    return Container(
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: ConstantsColors.blackShade900,
-            width: 1.0,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/notification_img.png",
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(height: 40),
+              Text(
+                'Suas notificações são exibidas aqui',
+                style: const TextStyle(
+                  color: ConstantsColors.blueShade900,
+                  fontSize: 20,
+                ).merge(TextStylesConstants.kpoppinsBold),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Não deixe passar nenhuma oportunidade de fazer o bem.',
+                style: const TextStyle(
+                  color: ConstantsColors.blueShade900,
+                  fontSize: 17,
+                ).merge(TextStylesConstants.kpoppinsRegular),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              TextButton(
+                onPressed: () {
+                  GoRouter.of(context).pushNamed("settingsPage");
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: ConstantsColors.blueShade900,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 12,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  'Ativar Notificações',
+                  style: const TextStyle(
+                    color: ConstantsColors.whiteShade900,
+                    fontSize: 16,
+                  ).merge(TextStylesConstants.kpoppinsMedium),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: alignment,
-        children: [
-          Expanded(
-            child: TextButton(
-              onPressed: onTap,
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.zero,
-                alignment: Alignment.centerLeft,
-              ),
-              child: Row(
-                mainAxisAlignment: alignment,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      color: textColor,
-                      fontSize: titleSize,
-                    ).merge(TextStylesConstants.kpoppinsLight),
-                  ),
-                  Icon(
-                    icon,
-                    color: iconColor,
-                    size: iconSize,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
