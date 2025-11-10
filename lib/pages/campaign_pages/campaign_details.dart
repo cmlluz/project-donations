0fe65d639f58e24883797e7687f057027ec771df
Luana Lima <luanalima.2006@hotmail.com>
2025-11-10 09:03:30 -0300

    feat: use complementary functions

--- lib/pages/campaign_pages/campaign_details.dart
+++ lib/pages/campaign_pages/campaign_details.dart
@@ -44,7 +44,9 @@ class _CampaignDetailsPageState extends State<CampaignDetailsPage> {
         final isLoading = campaignController.isLoading;
         final errorMessage = campaignController.errorMessage;
 
-        final bool isAuthor = campaign?.isAuthoredBy(userProvider.currentUser?.firebaseUid) ?? false;
+        final bool isAuthor =
+            campaign?.isAuthoredBy(userProvider.currentUser?.firebaseUid) ??
+                false;
 
         if (isLoading) {
           return const Scaffold(
