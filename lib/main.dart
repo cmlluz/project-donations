9b09071a7b140eee52a8d649f3a38353a05c81ba
Luana Lima <luanalima.2006@hotmail.com>
2026-08-04 21:55:44 -0300

    fix: register controllers

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,13 @@ import 'package:appdonationsgestor/controllers/user_provider.dart';
 import 'package:appdonationsgestor/controllers/campaign_controller.dart';
 import 'package:appdonationsgestor/controllers/donation_controller.dart';
 import 'package:appdonationsgestor/controllers/need_controller.dart';
-import 'package:appdonationsgestor/services/notification_service.dart';
 import 'firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(
-  RemoteMessage message,                                           
+  RemoteMessage message,
 ) async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -130,6 +129,12 @@ class _AppInitializationState extends State<AppInitialization> {
             ),
             ChangeNotifierProvider(
               create: (context) => CampaignController(),
+            ),
+            ChangeNotifierProvider(
+              create: (context) => DonationController(),
+            ),
+            ChangeNotifierProvider(
+              create: (context) => NeedController(),
             ),
           ],
           child: const MyApp(),
